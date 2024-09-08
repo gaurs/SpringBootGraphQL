ea264d05bb7cef3d264d28813a1bbbdcbf294cc3
Santhosh Krishnan <opendesk.ca@gmail.com>
2024-09-08 14:51:14 -0400

    Spring Security JWT Authentication.

--- src/main/java/com/accounts/config/AuthEntryPointJwt.java
+++ src/main/java/com/accounts/config/AuthEntryPointJwt.java
@@ -16,33 +16,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
-public class AuthEntryPointBasicAuth implements AuthenticationEntryPoint {
+public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
-    private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointBasicAuth.class);
+    private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
             throws IOException, ServletException {
-
-        // Check if the Authorization header is present
-        String authHeader = request.getHeader("Authorization");
-        if (authHeader == null || authHeader.isEmpty()) {
-            logger.error("Missing Authorization header");
-
-            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-
-            final Map<String, Object> body = new HashMap<>();
-            body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
-            body.put("error", "Unauthorized");
-            body.put("message", "Authorization header is missing in the request");
-            body.put("path", request.getServletPath());
-
-            final ObjectMapper mapper = new ObjectMapper();
-            mapper.writeValue(response.getOutputStream(), body);
-            return;
-        }
-
         logger.error("Unauthorized error: {}", authException.getMessage());
 
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
